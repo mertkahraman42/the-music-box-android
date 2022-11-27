5cd990b898f8e8c969055bb5625fda5c91a3e625
Mert Kahraman <mertkahraman42@gmail.com>
2022-11-27 22:29:55 +0300

    Move ApiService to api package

---
+++ app/src/main/java/com/mertkahraman/themusicbox/data/api/ApiService.kt
@@ -0,0 +1,13 @@
+package com.mertkahraman.themusicbox.data.api
+
+import com.mertkahraman.themusicbox.data.model.Artist
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+// If more MusicBrainz Entity types are added,
+// we can generalize lookup methods to a getEntity sort of call.
+interface ApiService {
+
+    @GET("/artist/{mbid}")
+    suspend fun getArtist(@Path("mbid") artistMbid: String): Artist
+}
