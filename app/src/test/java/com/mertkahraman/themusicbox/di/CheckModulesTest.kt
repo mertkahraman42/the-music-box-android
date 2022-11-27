e1d62a673f4c8cbe935a33c4eb68e55c67d467c9
Mert Kahraman <mertkahraman42@gmail.com>
2022-11-27 20:12:19 +0300

    Add Koin tests to verify config of modules

---
+++ app/src/test/java/com/mertkahraman/themusicbox/di/CheckModulesTest.kt
@@ -0,0 +1,25 @@
+package com.mertkahraman.themusicbox.di
+
+import com.mertkahraman.themusicbox.TmbApp
+import org.junit.Test
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.check.checkModules
+
+/*
+ * Test to verify configuration modules
+ */
+class CheckModulesTest : KoinTest {
+
+    @Test
+    fun `Test Koin Modules`() {
+        startKoin {
+            androidContext(TmbApp())
+            modules(listOf(appModule()))
+        }.checkModules()
+
+        stopKoin()
+    }
+}
