dcaf57b8c011a3dd661a3036bb53728fa13a8606
Mert Kahraman <mertkahraman42@gmail.com>
2022-11-27 20:11:57 +0300

    Define Application subclass, and start Koin with appModule

---
+++ app/src/main/java/com/mertkahraman/themusicbox/TmbApp.kt
@@ -0,0 +1,19 @@
+package com.mertkahraman.themusicbox
+
+import android.app.Application
+import com.mertkahraman.themusicbox.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class TmbApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@TmbApp)
+            modules(listOf(appModule()))
+        }
+    }
+}
