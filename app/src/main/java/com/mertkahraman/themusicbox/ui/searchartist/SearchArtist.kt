0c64fd5f8309d1b44d383428f28f740b91a46ad2
Mert Kahraman <mertkahraman42@gmail.com>
2022-11-29 23:06:30 +0300

    Include SearchView component in SearchArtist composable, update for dynamic Searching

--- app/src/main/java/com/mertkahraman/themusicbox/ui/searchartist/SearchArtist.kt
+++ app/src/main/java/com/mertkahraman/themusicbox/ui/searchartist/SearchArtist.kt
@@ -1,5 +1,6 @@
 package com.mertkahraman.themusicbox.ui.searchartist
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,6 +9,8 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import com.mertkahraman.themusicbox.data.model.Artist
+import com.mertkahraman.themusicbox.repo.paging.ArtistSource
+import com.mertkahraman.themusicbox.ui.components.EmptyListIndicator
 import com.mertkahraman.themusicbox.ui.components.ErrorListItem
 import com.mertkahraman.themusicbox.ui.components.FullscreenSpinner
 import com.mertkahraman.themusicbox.ui.components.SpinnerListItem
@@ -19,38 +22,59 @@ import org.koin.androidx.compose.getViewModel
 fun SearchArtist(
     viewModel: SearchArtistViewModel = getViewModel()
 ) {
-    val lazyArtistItems: LazyPagingItems<Artist> = viewModel.artistList.collectAsLazyPagingItems()
+    val searchTextState = viewModel.uiState.value
+    val lazyArtistItems: LazyPagingItems<Artist>? =
+        viewModel.getSearchResultStream(searchTextState.searchQuery)?.collectAsLazyPagingItems()
 
-    LazyColumn {
-        items(lazyArtistItems) { artist ->
-            ArtistItem(artist = artist!!)
+    Column {
+        SearchView(searchTextState) {
+            viewModel.onEvent(it)
         }
+        LazyColumn {
+            if (lazyArtistItems != null) {
+                items(lazyArtistItems) { artist ->
+                    ArtistItem(artist = artist!!)
+                }
 
-        lazyArtistItems.apply {
-            when {
-                loadState.refresh is LoadState.Loading -> {
-                    item { FullscreenSpinner(modifier = Modifier.fillParentMaxSize()) }
-                }
-                loadState.append is LoadState.Loading -> {
-                    item { SpinnerListItem() }
-                }
-                loadState.refresh is LoadState.Error -> {
-                    val e = lazyArtistItems.loadState.refresh as LoadState.Error
-                    item {
-                        ErrorListItem(
-                            modifier = Modifier.fillParentMaxSize(),
-                            errorMessage = e.error.localizedMessage!!,
-                            onRetry = { retry() }
-                        )
-                    }
-                }
-                loadState.append is LoadState.Error -> {
-                    val e = lazyArtistItems.loadState.append as LoadState.Error
-                    item {
-                        ErrorListItem(
-                            errorMessage = e.error.localizedMessage!!,
-                            onRetry = { retry() }
-                        )
+                lazyArtistItems.apply {
+                    when {
+                        loadState.refresh is LoadState.Loading -> {
+                            item { FullscreenSpinner(modifier = Modifier.fillParentMaxSize()) }
+                        }
+                        loadState.append is LoadState.Loading -> {
+                            item { SpinnerListItem() }
+                        }
+                        loadState.refresh is LoadState.Error -> {
+                            val loadStateError = lazyArtistItems.loadState.refresh as LoadState.Error
+                            when (loadStateError.error) {
+                                is ArtistSource.NoResultsException -> {
+                                    item {
+                                        EmptyListIndicator(
+                                            modifier = Modifier.fillParentMaxSize(),
+                                            query = searchTextState.searchQuery
+                                        )
+                                    }
+                                }
+                                else -> {
+                                    item {
+                                        ErrorListItem(
+                                            modifier = Modifier.fillParentMaxSize(),
+                                            errorMessage = loadStateError.error.localizedMessage!!,
+                                            onRetry = { retry() }
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        loadState.append is LoadState.Error -> {
+                            val e = lazyArtistItems.loadState.append as LoadState.Error
+                            item {
+                                ErrorListItem(
+                                    errorMessage = e.error.localizedMessage!!,
+                                    onRetry = { retry() }
+                                )
+                            }
+                        }
                     }
                 }
             }
