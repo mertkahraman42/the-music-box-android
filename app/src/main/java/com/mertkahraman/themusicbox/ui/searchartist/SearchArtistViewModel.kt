08189e10dfae6ececf2d77c6edbbc2465a5bc4a9
Mert Kahraman <mertkahraman42@gmail.com>
2022-11-29 23:05:23 +0300

    Remove dummy implementation on VM, add Paginated Search Artist logic, along with Search UI State/Event handlers

--- app/src/main/java/com/mertkahraman/themusicbox/ui/searchartist/SearchArtistViewModel.kt
+++ app/src/main/java/com/mertkahraman/themusicbox/ui/searchartist/SearchArtistViewModel.kt
@@ -1,18 +1,58 @@
 package com.mertkahraman.themusicbox.ui.searchartist
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.mertkahraman.themusicbox.data.model.Artist
 import com.mertkahraman.themusicbox.repo.Repository
 import com.mertkahraman.themusicbox.repo.paging.ArtistSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 class SearchArtistViewModel(
     private val repository: Repository
 ) : ViewModel() {
-    val artistList: Flow<PagingData<Artist>> = Pager(PagingConfig(pageSize = 10)) {
-        ArtistSource(repository, "the", 10)
-    }.flow
+
+    private var _searchUIState = mutableStateOf(SearchUIState())
+    val uiState: State<SearchUIState> = _searchUIState
+
+    fun onEvent(event: SearchUIEvent) {
+        viewModelScope.launch {
+            val searchQuery = when (event) {
+                is SearchUIEvent.SearchValueChanged -> event.searchQueryText
+                is SearchUIEvent.SearchUnfocused -> event.searchQueryText
+                is SearchUIEvent.SearchCleared -> ""
+            }
+            _searchUIState.value = _searchUIState.value.copy(
+                searchQuery = searchQuery
+            )
+
+            if (searchQuery != "")
+                getSearchResultStream(searchQuery)
+        }
+    }
+
+    fun getSearchResultStream(query: String): Flow<PagingData<Artist>>? {
+        return if (query == "") {
+            null
+        } else {
+            Pager(
+                PagingConfig(
+                    pageSize = DEFAULT_PAGE_SIZE
+                ),
+                pagingSourceFactory = {
+                    ArtistSource(repository, query, DEFAULT_PAGE_SIZE)
+                }
+            ).flow.cachedIn(viewModelScope)
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_PAGE_SIZE = 10
+    }
 }
